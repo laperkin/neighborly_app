d613ce259ef0ab9b1f57dba84c9ad7feb411d35c
Luke Perkins <laperkin@ncsu.edu>
2017-07-21 09:47:31 -0400

    scraping titles

--- neighborly_project/neighborly_app/CommercialRealestateScrape.py
+++ neighborly_project/neighborly_app/CommercialRealestateScrape.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 from geopy.geocoders import Nominatim
+from selenium import webdriver    #open webdriver for specific browser
+from selenium.webdriver.common.keys import Keys   # for necessary browser action
+from selenium.webdriver.common.by import By    # For selecting html code
+import time
 
 geolocator = Nominatim()
 
@@ -16,7 +20,9 @@ lat = location.latitude
 lon = location.longitude
 
 
-url= "http://www.cityfeet.com/cont/{}/{}-commercial-real-estate?lat={}&lng=-{}#".format(state,city,lat,lon)
+
+
+url= "http://www.cityfeet.com/cont/{}/{}-retail-space?lat={}&lng=-{}#".format(state,city,lat,lon)
 headers = {'User-Agent':'Mozilla/5.0'}
 
 
@@ -26,7 +32,8 @@ with requests.Session() as s:
     g = s.get(url)
     soup = BeautifulSoup(g.text, "html.parser")
     soup.pretify
-    listings = soup.find_all("div", class_="property clearfix src-LN level-30")
-    print(listings)
-
+    listings = soup.find_all("div", {'class':"property clearfix src-LN level-30",'class':"property clearfix src-LN level-20"})
+    for entry in listings:
+        print(entry.find("div",{"class":"row-fluid"}).a.contents)
+            
     
